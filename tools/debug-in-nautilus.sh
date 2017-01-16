459c56c8e16c73cd9ce5e01d386ac81d36311cab
Fabien LOISON <flo@flogisoft.com>
2017-01-16 13:13:38 +0100

    debug-in-nautilus-.sh: allows to keep the process attached

--- tools/debug-in-nautilus.sh
+++ tools/debug-in-nautilus.sh
@@ -6,4 +6,8 @@ export GDK_DEBUG=interactive
 export NAUTILUS_TERMINAL_DEBUG=true
 export NAUTILUS_TERMINAL_DEBUG_PACKAGE_PATH="$PWD"
 
-nautilus "$PWD" &
+if [ "$1" == "--no-bg" ] ; then
+    nautilus "$PWD"
+else
+    nautilus "$PWD" &
+fi
